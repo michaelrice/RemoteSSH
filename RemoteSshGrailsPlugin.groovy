14a143568e13939ae692d1b1a24dde6d865a2a8d
Michael Rice <michael.rice@rackspace.com>
2014-05-10 02:04:10 -0500

    added me as a developer and tabs to spaces

--- RemoteSshGrailsPlugin.groovy
+++ RemoteSshGrailsPlugin.groovy
@@ -1,20 +1,26 @@
 import grails.plugin.remotessh.SshConfig
 
 class RemoteSshGrailsPlugin {
-	def version = "0.2"
-	def grailsVersion = "2.0 > *"
-	def title = "Remote SSH Plugin"
-	def author = "Vahid Hedayati"
-	def authorEmail = "badvad@gmail.com"
-	def description = 'Uses the Ganymed SSH-2 library to provide RemoteSSH, RemoteSCP, RemoteSCPDir, and RemoteSCPGet'
-	def documentation = "http://grails.org/plugin/remote-ssh"
-	def license = "GPL2"
-	def issueManagement = [system: 'GITHUB', url: 'https://github.com/vahidhedayati/RemoteSSH/issues']
-	def scm = [url: 'https://github.com/vahidhedayati/RemoteSSH']
+    def version = "0.2"
+    def grailsVersion = "2.0 > *"
+    def title = "Remote SSH Plugin"
+    def author = "Vahid Hedayati"
+    def authorEmail = "badvad@gmail.com"
+    def developers = [
+            [
+                    name: "Michael Rice",
+                    email: "michael@michaelrice.org"
+            ],
+    ]
+    def description = 'Uses the Ganymed SSH-2 library to provide RemoteSSH, RemoteSCP, RemoteSCPDir, and RemoteSCPGet'
+    def documentation = "http://grails.org/plugin/remote-ssh"
+    def license = "GPL2"
+    def issueManagement = [system: 'GITHUB', url: 'https://github.com/vahidhedayati/RemoteSSH/issues']
+    def scm = [url: 'https://github.com/vahidhedayati/RemoteSSH']
 
-	def doWithSpring = {
-		sshConfig(SshConfig) {
-			grailsApplication = ref('grailsApplication')
-		}
-	}
+    def doWithSpring = {
+        sshConfig(SshConfig) {
+            grailsApplication = ref('grailsApplication')
+        }
+    }
 }
